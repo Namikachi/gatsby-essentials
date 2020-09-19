033e348ac555afb4b8c3d9ccdddd1063a9c4020d
Panorami <loud.music.n@gmail.com>
2020-09-19 11:20:47 +0900

    footer画像の最適化

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,6 +108,9 @@ export default ({ data }) => (
           </li>
         </ul>
       </div>
+      <div className="back">
+        <Img fluid={data.pattern.childImageSharp.fluid} alt="" style={{height: "100%"}} />
+      </div>
     </footer>
   </div>
 )
@@ -145,6 +148,13 @@ export const query = graphql`
     berry: file(relativePath: {eq: "berry.jpg"}) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    pattern: file(relativePath: {eq: "pattern.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1920, quality: 90) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
